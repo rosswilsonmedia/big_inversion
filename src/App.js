7978b1fa4a1f1923069e982d99fd5949d3de0d4c
rosswilsonmedia <82285604+rosswilsonmedia@users.noreply.github.com>
2021-07-06 10:38:02 -0700

    PersonCard component addition and styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 
+import PersonCard from './components/PersonCard';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <PersonCard lastName="Doe" firstName="Jane" age={45} hairColor="Black"/>
+      <PersonCard lastName="Smith" firstName="John" age={88} hairColor="Brown"/>
+      <PersonCard lastName="Fillmore" firstName="Millard" age={50} hairColor="Brown"/>
+      <PersonCard lastName="Smith" firstName="Maria" age={62} hairColor="Brown"/>
     </div>
   );
 }
